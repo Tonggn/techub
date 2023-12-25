5888e4232430ad468ec60e039ca29e459314da62
HyeonjaeKwon <kwonhyeonjae01@gmail.com>
2023-12-25 23:27:09 +0900

    feat: Publisher 엔티티 구현

---
+++ server/src/main/java/com/tonggn/techub/publisher/Publisher.java
@@ -0,0 +1,25 @@
+package com.tonggn.techub.publisher;
+
+import com.tonggn.techub.base.BaseDatetime;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class Publisher extends BaseDatetime {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  private String name;
+
+  private String rssLink;
+
+  private String imagePath;
+}
