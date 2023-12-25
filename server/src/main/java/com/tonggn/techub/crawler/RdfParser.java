7d488e89afd702c2fc31a12d44158f9c0693702d
HyeonjaeKwon <kwonhyeonjae01@gmail.com>
2023-12-25 20:36:00 +0900

    feat: Rss 1.0 Parser 구현

---
+++ server/src/main/java/com/tonggn/techub/crawler/RdfParser.java
@@ -0,0 +1,26 @@
+package com.tonggn.techub.crawler;
+
+import java.util.List;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+
+class RdfParser {
+
+  private RdfParser() {
+  }
+
+  public static List<ParseResponse> parse(final Document xml) {
+    return xml.select("channel > item").stream()
+        .map(RdfParser::mapToResponse)
+        .toList();
+  }
+
+  private static ParseResponse mapToResponse(final Element item) {
+    final String title = ParserUtils.selectFirstTextOrEmpty(item, "title");
+    final String link = ParserUtils.selectFirstTextOrEmpty(item, "link");
+    final String description = ParserUtils.selectFirstTextOrEmpty(item, "description");
+    final String pubDate = ParserUtils.selectFirstTextOrEmpty(item, "dc:date");
+    final String image = ParserUtils.selectFirstTextOrEmpty(item, "image");
+    return new ParseResponse(title, link, description, pubDate, image);
+  }
+}
