6c1f953e4ac1945d524d1e4b364156df20cc1669
HyeonjaeKwon <kwonhyeonjae01@gmail.com>
2023-12-25 23:27:15 +0900

    feat: Feed 엔티티 구현

---
+++ server/src/main/java/com/tonggn/techub/feed/Feed.java
@@ -0,0 +1,35 @@
+package com.tonggn.techub.feed;
+
+import com.tonggn.techub.base.BaseDatetime;
+import com.tonggn.techub.publisher.Publisher;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Feed extends BaseDatetime {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @ManyToOne
+  private Publisher publisher;
+
+  private String title;
+
+  private String link;
+
+  private String description;
+
+  private String imageUrl;
+
+  private String pubDate;
+}
