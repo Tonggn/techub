79510a1c565b6dab0a52a05111247c5b8fee26af
hyeonjerry <kwonhyeonjae01@gmail.com>
2024-01-03 21:23:34 +0900

    refactor: Publisher Repository 페이지네이션 리팩터링

--- server/src/main/java/com/tonggn/techub/publisher/PublisherRepository.java
+++ server/src/main/java/com/tonggn/techub/publisher/PublisherRepository.java
@@ -1,14 +1,9 @@
 package com.tonggn.techub.publisher;
 
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface PublisherRepository extends CrudRepository<Publisher, Long> {
+public interface PublisherRepository extends JpaRepository<Publisher, Long> {
 
-  @Query
-  Page<Publisher> findAllByOrderByCreatedAtDesc(Pageable pageable);
 }
--- server/src/main/java/com/tonggn/techub/publisher/PublisherService.java
+++ server/src/main/java/com/tonggn/techub/publisher/PublisherService.java
@@ -10,6 +10,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
@@ -33,8 +34,8 @@ public class PublisherService {
   }
 
   public Page<PublisherResponse> findPageByLatest(final Integer page, final Integer size) {
-    final Pageable pageable = PageRequest.of(page, size);
-    final Page<Publisher> publishers = publisherRepository.findAllByOrderByCreatedAtDesc(pageable);
+    final Pageable pageable = PageRequest.of(page, size, Sort.by("createdAt").descending());
+    final Page<Publisher> publishers = publisherRepository.findAll(pageable);
 
     final List<PublisherResponse> publisherResponses = publishers.stream()
         .map(PublisherResponse::from)
