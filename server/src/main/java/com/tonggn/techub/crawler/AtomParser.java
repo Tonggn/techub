3a84524236b36c1aea10a40ed7e82c0427af705c
hyeonjerry <kwonhyeonjae01@gmail.com>
2024-01-04 22:13:55 +0900

    refactor: 파싱된 피드 DTO명 수정

--- server/src/main/java/com/tonggn/techub/crawler/AtomParser.java
+++ server/src/main/java/com/tonggn/techub/crawler/AtomParser.java
@@ -9,18 +9,18 @@ class AtomParser {
   private AtomParser() {
   }
 
-  public static List<ParseResponse> parse(final Document xml) {
+  public static List<ParsedFeed> parse(final Document xml) {
     return xml.select("feed > entry").stream()
         .map(AtomParser::mapToResponse)
         .toList();
   }
 
-  private static ParseResponse mapToResponse(final Element item) {
+  private static ParsedFeed mapToResponse(final Element item) {
     final String title = ParserUtils.selectFirstTextOrEmpty(item, "title");
     final String link = ParserUtils.selectFirstAttrOrEmpty(item, "link", "href");
     final String description = ParserUtils.selectFirstTextOrEmpty(item, "summary");
     final String pubDate = ParserUtils.selectFirstTextOrEmpty(item, "updated");
     final String image = ParserUtils.selectFirstTextOrEmpty(item, "image");
-    return new ParseResponse(title, link, description, pubDate, image);
+    return new ParsedFeed(title, link, description, pubDate, image);
   }
 }
